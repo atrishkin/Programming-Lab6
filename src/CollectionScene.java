b6e7b4dcc1b923833f1126c513788fa94ce65b95
tonches <anton_trishkin@mail.ru>
2017-10-20 01:56:42 +0300

    Update table: add header and scrolling panel

--- src/CollectionScene.java
+++ src/CollectionScene.java
@@ -1,24 +1,31 @@
-import javax.swing.*;
-import java.awt.*;
-import java.util.ArrayList;
+import sun.swing.table.DefaultTableCellHeaderRenderer;
 
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
+import java.awt.*;
 public class CollectionScene {
     private static JPanel Scene2 = new JPanel(new GridLayout(2, 1, 10,10));
-
+    private static String[] coloumNames = {"Номер","Название","Литраж","Год","Приоритет"};
     private static Storage storage = new Storage();
-    private static String[] header = new String[] {"Номер","Название","Литраж","Год","Приоритет"};
+
     //TODO: tab2
     public static JPanel addScene2(){
+        //TODO: запись из файла, может в сцене 1
+        //Probe content!!!
+        for (int i = 0; i < 7; i++) {
+            storage.addJam(new Jam("Малина",2.5,2014,2));
+            storage.addJam(new Jam("Морошка",1,2016,1));
+            storage.addJam(new Jam("Черника",3,2014,2));
+            storage.addJam(new Jam("Морожка",1,2015,1));
+        }
         //
-        storage.addJam(new Jam("Малина",2.5,2014,2));
-        storage.addJam(new Jam("Морожка",1,2016,1));
-        storage.addJam(new Jam("Черника",3,2014,2));
-        storage.addJam(new Jam("Морожка",1,2015,1));
-        //
-        JTable collectionTable = new JTable(storage.getContent(), header);
-        collectionTable.setVisible(true);
+        JTable collectionTable = new JTable(storage.getContent(), coloumNames);
+        JTableHeader header = collectionTable.getTableHeader();
+        header.setDefaultRenderer(new DefaultTableCellHeaderRenderer());
+        collectionTable.setEnabled(false);
 
-        Scene2.add(collectionTable);
+        Scene2.add(new JScrollPane(collectionTable));
         Scene2.add(new JPanel());
         return Scene2;
     }
--- src/Storage.java
+++ src/Storage.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Stack;
 
 public class Storage {
     private int max = -1;
@@ -31,9 +30,9 @@ public class Storage {
         return jamStorage;
     }
     Object[][] getContent(){
-        Object[][] content = new Object[jamStorage.size()][5];
+        Object[][] content = new Object[jamStorage.size()+1][5];
         for (int i = 0; i < jamStorage.size(); i++) {
-            content[i][0] = i;
+            content[i][0] = i+1;
             content[i][1] = jamStorage.get(i).getName();
             content[i][2] = jamStorage.get(i).getSize();
             content[i][3] = jamStorage.get(i).getYear();
